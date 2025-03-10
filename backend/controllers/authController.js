f43490625a50028fef1efcd7475ec3f569205a6d
kingpriyanshu21 <priyanshusingh8233@gmail.com>
2025-03-10 10:44:58 +0530

    update code

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -32,7 +32,7 @@ export const register=async (req,res)=>{
             httpOnly:true,
             secure:process.env.NODE_ENV==='production',
             sameSite:process.env.NODE_ENV==='production' ? 'none' : 'strict',
-            maxAge:7*24*60*60*100
+            maxAge:7*24*60*60*1000
 
         })
 
@@ -91,7 +91,7 @@ export const login=async (req,res)=>{
             httpOnly:true,
             secure:process.env.NODE_ENV==='production',
             sameSite:process.env.NODE_ENV==='production' ? 'none' : 'strict',
-            maxAge:7*24*60*60*100
+            maxAge:7*24*60*60*1000
 
         })
 
